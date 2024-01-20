9b95308242c84d09c6bf00697c8ae82f52a4fc2c
Zodecok <118003544+Zodecok@users.noreply.github.com>
2024-01-20 20:42:27 +1000

    Did not do any commits. Just doing stuff
    It is all done, not well but it is done

--- console-player.js
+++ console-player.js
@@ -46,6 +46,7 @@ function round(playerSelection, computerSelection) {
 }
 
 function valueFromWord(word) {
+    
     // default is rock
     let value = 0;
     if (word.toLowerCase() === "paper") {
@@ -71,10 +72,54 @@ function wordFromValue(value) {
     return word;
 }
 
+// need to have a function that runs that whole game
+// and give that to each of the buttons to generate
+// a computer and then do the assignment
+
+function btnSetUp(e) {
+    let result = round(e.target.id, getComputerChoice());
+    console.log(results);
+    let log = "messed";
+    // 0 is tie, 1 is player, -1 is computer
+    switch (result) {
+        case 0:
+            log = "This was a tie in btnset";
+            results["ties"] += 1;
+            break;
+        case 1:
+            log = "This is a win for player in btnset";
+            results["wins"] += 1;
+            break;
+            
+        case -1:
+            log = "This is a loss for player in btnset";
+            results["losses"] += 1;
+            break;
+        default:
+            log = "This did not work in btnset";
+            alert("something didn't work");
+    }
+    document.querySelector("#output").textContent = log;
+    update();
+}
+
+function update() {
+    const spans = Array.from(document.querySelectorAll("div>span.results"));
+    spans.forEach((el, i) => el.textContent = Object.values(results)[i]);
+    spans.forEach((element, index) => console.log(`el:${element}, index:${index}`));
+    Object.values(results).forEach(value => {
+        if (value >= maxGames) {
+            document.querySelector("#output").textContent = "Game Finished";
+            Array.from(document.querySelectorAll("button")).forEach(btn => btn.disabled = true);
+        };
+    });
+}
+
 function game() {
+
     let score = [0, 0];
     // player then computer
-    for (let i = 0; i < 2; i++) {
+    while (score[0] < 5 || score[1] < 5) {
         let result = 0;
         while (result == 0) {
             let playerSelection = prompt("What is your play?", "Error");
@@ -86,3 +131,7 @@ function game() {
     console.log(`Player has won: ${score[0]} times\nComputer has won: ${score[1]} times`);
 }
 
+let results = {"wins": 0, "ties": 0, "losses": 0};
+let maxGames = 5;
+const btns = document.querySelectorAll("button");
+btns.forEach(btn => btn.addEventListener('click', btnSetUp));
